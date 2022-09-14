b4e2e14be8498b1a781a1f31312144149aaf2532
kimkunndad <kim.kundad@gmail.com>
2022-09-15 03:41:22 +0700

    kim

---
+++ database/migrations/2020_07_05_183258_create_sms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subject')->nullable();
+            $table->text('message')->nullable();
+            $table->string('date_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
